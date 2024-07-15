60fcc21be7c21dd08c06efce0b5867874c1a4d91
Jaron Leach <jaronleach98@gmail.com>
2024-07-15 10:52:59 -0400

    starting filling out the fighting the monsters functions.

--- script.js
+++ script.js
@@ -144,8 +144,18 @@ function sellWeapon() {
   }
 }
 
-function fightSlime() {}
-function fightBeast() {}
-function fightDragon() {
-  console.log("Fighting dragon.");
+function fightSlime() {
+  fighting = 0;
+  goFight();
 }
+function fightBeast() {
+  fighting = 1;
+  goFight();
+}
+
+function fightDragon() {
+  fighting = 2;
+  goFight();
+}
+
+function goFight() {}
